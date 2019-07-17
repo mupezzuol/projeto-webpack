a2b86a7010d04e1d66d248cbb422b7bd43248f2d
Murillo Pezzuol <murillo.pezzuol@kaspper.com>
2019-07-17 11:54:10 -0300

    Criando build pra Prod

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,6 +14,15 @@ module -> Adiciono módulo, dentro dele regras, posso ter várias regras (rules)
 //Módulo do webpack para nos retornar o caminho de onde está o módulo
 // __dirname -> Pega o caminho do módulo e concatena com o 'dist' passado, montando o path que nós importaremos no 'index.html'
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');//Plugin do babili
+
+//Verificando a variavel setado na hora da execução, se for prod nós criamos um plugin e adicionamos nas configs do module
+//dessa forma fazemos build de prod com arquios minificados etc...
+let plugins = [];
+if(process.env.NODE_ENV == 'production') {
+    plugins.push(new babiliPlugin());
+}
+
 
 //Criando nosso módulo do webpack passando as configurações em forma de objeto
 module.exports = {
@@ -32,5 +41,6 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins
 }
